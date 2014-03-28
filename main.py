b65f01a4a1095b314fada4a448efc1a07ee7fd60
Cesar <cesar.chavez@petrologautomation.com>
2014-03-27 21:49:44 -0500

    Improved logging in main.py Delayed thread execution Catched general exception in main.py

--- main.py
+++ main.py
@@ -9,25 +9,33 @@ import comunicacionG4
 import actuaEventos
 import adquiereEventos
 import verificaRelojSistema
+import time
 
-
+config.logging.info("-------------Starting Threads-------------")
 # Start Adquiere Eventos Daemon
 adquiereEventosDaemon = threading.Thread(target=adquiereEventos.adquiereEventos)
 adquiereEventosDaemon.daemon = True
 adquiereEventosDaemon.start()
+time.sleep(.5)
 # Start Actua Eventos Daemon
 actuaEventosDaemon = threading.Thread(target=actuaEventos.actuaEventos)
 actuaEventosDaemon.daemon = True
 actuaEventosDaemon.start()
+time.sleep(.5)
 # Start Serial Coms Daemon
 serialDaemon = threading.Thread(target=comunicacionG4.serialDaemon)
 serialDaemon.daemon = True
 serialDaemon.start()
+time.sleep(.5)
 # Start Time Verification Daemon
 verificaTiempoDaemon = threading.Thread(target=verificaRelojSistema.comparaTiempos)
 verificaTiempoDaemon.daemon = True
 verificaTiempoDaemon.start()
+time.sleep(.5)
+config.logging.info("---------Starting Threads Done!-----------")
 
 while True:
-    a = 0
-
+    try:
+        a = 0
+    except Exception as e:
+        config.logging.error("main: Unexpected exception - {}".format(e))
